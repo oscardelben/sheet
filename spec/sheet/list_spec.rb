7c227fd18142520ef1b50f66257e0bf0ab5acd4b
Oscar Del Ben <info@oscardelben.com>
2012-04-09 10:47:32 +0200

    Fix ls expectation

--- spec/sheet/list_spec.rb
+++ spec/sheet/list_spec.rb
@@ -5,7 +5,7 @@ describe Sheet::List do
 
   it 'calls system ls' do
     cmd = "ls #{Sheet.sheets_dir}"
-    Sheet.should_receive(:exec).with(cmd)
+    Sheet.should_receive(:exec).with(cmd, true)
 
     Sheet::List.new.list
   end

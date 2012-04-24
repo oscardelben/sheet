4c4bd24b9245fa1eb7475deacda69924ab3262fa
Oscar Del Ben <info@oscardelben.com>
2012-04-24 10:39:38 +0200

    Fix list expectation

--- spec/sheet/list_spec.rb
+++ spec/sheet/list_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe Sheet::List do
 
   it 'calls system ls' do
-    cmd = "ls #{Sheet.sheets_dir}"
+    cmd = "ls -1 #{Sheet.sheets_dir}"
     Sheet.should_receive(:exec).with(cmd, true)
 
     Sheet::List.new.list
